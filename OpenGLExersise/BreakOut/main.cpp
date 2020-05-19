1f70e3d198155d8b48cbd6d2ad7ecb2baad561ab
leon <lyuleon@163.com>
2020-05-19 10:50:44 +0800

    init breakout add font render

---
+++ OpenGLExersise/BreakOut/main.cpp
@@ -0,0 +1,14 @@
+#include "ft2build.h"
+#include FT_FREETYPE_H 
+#include <iostream>
+
+int main()
+{
+	FT_Library ft;
+	if (FT_Init_FreeType(&ft))
+		std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
+
+	FT_Face face;
+	if (FT_New_Face(ft, "fonts/arial.ttf", 0, &face))
+		std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+}
--- OpenGLExersise/OpenGLExersise/main.cpp
+++ OpenGLExersise/OpenGLExersise/main.cpp
@@ -7,6 +7,9 @@
 #include <random>
 #include "Camera.h"
 #include "model.h"
+// FreeType
+#include <ft2build.h>
+#include FT_FREETYPE_H
 
 #pragma region Camera Instance
 const unsigned int SCR_WIDTH = 800;
@@ -124,6 +127,15 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 }
 #pragma  endregion 
 
+/// Holds all state information relevant to a character as loaded using FreeType
+struct Character {
+	GLuint TextureID;   // ID handle of the glyph texture
+	glm::ivec2 Size;    // Size of glyph
+	glm::ivec2 Bearing;  // Offset from baseline to left/top of glyph
+	GLuint Advance;    // Horizontal offset to advance to next glyph
+};
+std::map<GLchar, Character> Characters;
+
 unsigned int loadImageToGPU(const char*filename, GLint internalFormat, GLenum format)
 {
 	unsigned int TexBuffer;
@@ -233,8 +245,6 @@ unsigned int loadCubemap(vector<std::string> faces)
 
 GLFWwindow* GenerateWindow(const char* windowName)
 {
-
-#pragma region Open Window
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -256,7 +266,6 @@ GLFWwindow* GenerateWindow(const char* windowName)
 	glfwSetCursorPosCallback(window, mouse_callback);
 	glfwSetScrollCallback(window, scroll_callback);
 	return window;
-#pragma endregion 
 
 }
 
@@ -563,6 +572,47 @@ void renderScenePointShadow(const Shader &shader)
 	model = glm::scale(model, glm::vec3(0.75f));
 	shader.setMat4("model", model);
 	renderCube();
+}
+#pragma endregion
+
+#pragma region RenderText
+void RenderText(Shader &s, std::string text, GLfloat x, GLfloat y, GLfloat scale, glm::vec3 color,int VBO)
+{
+	s.use();
+	s.setVec3("textColor", color);
+	glActiveTexture(GL_TEXTURE0);
+	// 遍历文本中所有的字符
+	std::string::const_iterator c;
+	for (c = text.begin(); c != text.end(); c++)
+	{
+		Character ch = Characters[*c];
+		GLfloat xpos = x + ch.Bearing.x * scale;
+		GLfloat ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
+
+		GLfloat w = ch.Size.x * scale;
+		GLfloat h = ch.Size.y * scale;
+		// 对每个字符更新VBO
+		GLfloat vertices[6][4] = {
+			{ xpos,     ypos + h,   0.0, 0.0 },
+			{ xpos,     ypos,       0.0, 1.0 },
+			{ xpos + w, ypos,       1.0, 1.0 },
+
+			{ xpos,     ypos + h,   0.0, 0.0 },
+			{ xpos + w, ypos,       1.0, 1.0 },
+			{ xpos + w, ypos + h,   1.0, 0.0 }
+		};
+		// 在四边形上绘制字形纹理
+		glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+		// 更新VBO内存的内容
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		// 绘制四边形
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		// 更新位置到下一个字形的原点，注意单位是1/64像素
+		x += (ch.Advance >> 6) * scale; // 位偏移6个单位来获取单位为像素的值 (2^6 = 64)
+	}
+	glBindTexture(GL_TEXTURE_2D, 0);
 }
 #pragma endregion
 
@@ -6619,6 +6669,104 @@ int SSAO()
 	return 0;
 }
 
+int RenderText_0()
+{
+	//open window
+	GLFWwindow* window = GenerateWindow("RenderText_0");
+	if (window == nullptr) { return -1; }
+	//init shader
+	Shader shader("shaders/Text/textSource.vert", "shaders/Text/textSource.frag");
+	glm::mat4 projection = glm::ortho(0.0f, 800.0f, 0.0f, 600.0f);
+	shader.use();
+	shader.setMat4("projection", projection);
+	//load model+texture
+	FT_Library ft;
+	if (FT_Init_FreeType(&ft))
+		std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
+
+	FT_Face face;
+	if (FT_New_Face(ft, "fonts/arial.ttf", 0, &face))
+		std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+	// Set size to load glyphs as
+	FT_Set_Pixel_Sizes(face, 0, 48);
+	//禁用字节对齐限制
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	for (GLubyte c = 0; c < 128; c++)
+	{
+		// 加载字符的字形 
+		if (FT_Load_Char(face, c, FT_LOAD_RENDER))
+		{
+			std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+			continue;
+		}
+		// 生成纹理
+		GLuint texture;
+		glGenTextures(1, &texture);
+		glBindTexture(GL_TEXTURE_2D, texture);
+		glTexImage2D(
+			GL_TEXTURE_2D,
+			0,
+			GL_RED,
+			face->glyph->bitmap.width,
+			face->glyph->bitmap.rows,
+			0,
+			GL_RED,
+			GL_UNSIGNED_BYTE,
+			face->glyph->bitmap.buffer
+		);
+		// 设置纹理选项
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		// 储存字符供之后使用
+		Character character = {
+			texture,
+			glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+			glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+			face->glyph->advance.x
+		};
+		Characters.insert(std::pair<GLchar, Character>(c, character));
+	}
+	FT_Done_Face(face);
+	FT_Done_FreeType(ft);
+	//init VAO VBO
+	GLuint VAO, VBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), 0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	glEnable(GL_CULL_FACE);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	//render loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// input处理输入放在最前,原因是glfwPollEvents需要消耗不少时间
+		processInput(window);
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//render ...
+		glBindVertexArray(VAO);
+		RenderText(shader, "This is sample text", 25.0f, 25.0f, 1.0f, glm::vec3(0.5, 0.8f, 0.2f),VBO);
+		RenderText(shader, "(C) LearnOpenGL.com", 540.0f, 570.0f, 0.5f, glm::vec3(0.3, 0.7f, 0.9f),VBO);
+		glBindVertexArray(0);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
 int main() {
-	return SSAO();
+	return RenderText_0();
 }
